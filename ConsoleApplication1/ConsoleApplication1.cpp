8204c4de79f9edffa0d225743f548b7693a458ce
ftrovao <60818298+ftrovao@users.noreply.github.com>
2025-07-02 10:31:00 -0400

    Update ConsoleApplication1.cpp
    modification pour pull

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "second commit!\n";
+    std::cout << "modification!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
